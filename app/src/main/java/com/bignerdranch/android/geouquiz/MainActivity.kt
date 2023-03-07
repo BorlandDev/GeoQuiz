72aaf0812ad808520d8193733637153180a7c005
BorlandDev <minsk1115596@gmail.com>
2023-03-07 18:25:09 +0200

    finished first chapter

--- app/src/main/java/com/bignerdranch/android/geouquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geouquiz/MainActivity.kt
@@ -2,10 +2,34 @@ package com.bignerdranch.android.geouquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bignerdranch.android.geouquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding
+            ?: throw IllegalStateException("MainActivity binding == null in ${lifecycle.currentState}")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.run {
+            trueButton.setOnClickListener {
+                Toast.makeText(this@MainActivity, R.string.correct_toast, Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            falseButton.setOnClickListener {
+                Toast.makeText(this@MainActivity, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 }
